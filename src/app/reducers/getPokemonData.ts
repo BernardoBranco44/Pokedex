a17e6ebe7eb237b2ee1babcc0c9ae26821ba3123
Bernardo Branco <bernardomspbranco@gmail.com>
2023-05-01 22:44:24 +0100

    s

--- src/app/reducers/getPokemonData.ts
+++ src/app/reducers/getPokemonData.ts
@@ -3,37 +3,42 @@ import { generatedPokemonType, genericPokemonType } from "../../utils/Types";
 import axios from "axios";
 import { defaultImages, images } from "../../utils/getPokemonImages";
 
-export const getPokemonData = createAsyncThunk("pokemon/randomPokemon",async (pokemons:genericPokemonType[]) => {
-  try {
-    const pokemonsData: generatedPokemonType[] = [];
-    for await (const pokemon of pokemons) {
-      const {data}:{
-        data:{
-          id: number;
-          types: {type: generatedPokemonType}[];
-        };
-      } = await axios.get(pokemon.url)
-      const types = data.types.map(({ type:{name} }:{type:{name:string}}) => ({
-        [name]:pokemonTypes[name]
-      }));
-      // @ts-expect-error
-      let image: string = images[data.id]
-      if (!image) {
+export const getPokemonData = createAsyncThunk(
+  "pokemon/randomPokemon",
+  async (pokemons: genericPokemonType[]) => {
+    try {
+      const pokemonsData: generatedPokemonType[] = [];
+      for await (const pokemon of pokemons) {
+        const {
+          data,
+        }: {
+          data: {
+            id: number;
+            types: { type: generatedPokemonType }[];
+          };
+        } = await axios.get(pokemon.url);
+        const types = data.types.map(
+          ({ type: { name } }: { type: { name: string } }) => ({
+            [name]: pokemonTypes[name],
+          })
+        );
         // @ts-expect-error
-        image = defaultImages[data.id]
-      }
-      if (image) {
-        pokemonsData.push({
-          name: [pokemon.name],
-          id: data.id,
-          image,
-          types,
-        })
-      }
+        let image: string = images[data.id];
+        if (!image) {
+          // @ts-expect-error
+          image = defaultImages[data.id];
+        }
+        if (image) {
+          pokemonsData.push({
+            name: [pokemon.name],
+            id: data.id,
+            image,
+            types,
+          });
+        }
 
-      console.log({data})
-    }
-  } catch (err) {
-
+        console.log({ data });
+      }
+    } catch (err) {}
   }
-})
+);
